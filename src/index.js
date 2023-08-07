90fd2e927b9adacde147612f931ac117b7c430c8
katiemayjones <katiemayjones1990@gmail.com>
2023-08-07 21:38:20 +0100

    Added current times for four cities + conversion between 24hr and 12hr clock

--- src/index.js
+++ src/index.js
@@ -8,12 +8,19 @@ currentDate.innerHTML = moment()
   .tz(`${currentLocation}`)
   .format("dddd  Do MMMM YYYY");
 
-let currentTime = document.querySelector("#current-time");
-currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:MM");
+twentyFourHour();
 
 function twentyFourHour(event) {
   let currentTime = document.querySelector("#current-time");
-  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("h:mm a");
+  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:MM");
+  let cityOne = document.querySelector("#time-one");
+  cityOne.innerHTML = moment().tz("Asia/Beirut").format("HH:MM");
+  let cityTwo = document.querySelector("#time-two");
+  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("HH:MM");
+  let cityThree = document.querySelector("#time-three");
+  cityThree.innerHTML = moment().tz("America/Mexico_City").format("HH:MM");
+  let cityFour = document.querySelector("#time-four");
+  cityFour.innerHTML = moment().tz("Asia/Beijing").format("HH:MM");
 }
 
 let twentyFour = document.querySelector("#twenty-four");
@@ -21,7 +28,15 @@ twentyFour.addEventListener("click", twentyFourHour);
 
 function twelveHour(event) {
   let currentTime = document.querySelector("#current-time");
-  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:MM");
+  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("h:mm a");
+  let cityOne = document.querySelector("#time-one");
+  cityOne.innerHTML = moment().tz("Asia/Beirut").format("h:mm a");
+  let cityTwo = document.querySelector("#time-two");
+  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("h:mm a");
+  let cityThree = document.querySelector("#time-three");
+  cityThree.innerHTML = moment().tz("America/Mexico_City").format("h:mm a");
+  let cityFour = document.querySelector("#time-four");
+  cityFour.innerHTML = moment().tz("Asia/Beijing").format("h:mm a");
 }
 
 let twelve = document.querySelector("#twelve");
