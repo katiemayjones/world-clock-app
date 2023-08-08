c85e9ee5b30488fddbc7190eb541eb04bec2f3d7
katiemayjones <katiemayjones1990@gmail.com>
2023-08-08 18:25:55 +0100

    Changed selectors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 let currentLocation = moment.tz.guess();
 
 let currentCity = document.querySelector("#current-city");
-currentCity.innerHTML = `${currentLocation}`;
+currentCity.innerHTML = `${currentLocation.replace("_", " ").split("/")[1]}`;
 
 let currentDate = document.querySelector("#current-date");
 currentDate.innerHTML = moment()
@@ -12,18 +12,18 @@ twentyFourHour();
 
 function twentyFourHour(event) {
   let currentTime = document.querySelector("#current-time");
-  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:mm:ss");
+  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:mm");
 
   let cityOne = document.querySelector("#time-one");
   let cityOneDate = document.querySelector("#city-one-date");
-  cityOne.innerHTML = moment().tz("Asia/Beirut").format("HH:mm:ss");
+  cityOne.innerHTML = moment().tz("Asia/Beirut").format("HH:mm");
   cityOneDate.innerHTML = moment()
     .tz("Asia/Beirut")
     .format("dddd  Do MMMM YYYY");
 
   let cityTwo = document.querySelector("#time-two");
   let cityTwoDate = document.querySelector("#city-two-date");
-  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("HH:mm:ss");
+  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("HH:mm");
   cityTwoDate.innerHTML = moment()
     .tz("Europe/Sofia")
     .format("dddd  Do MMMM YYYY");
@@ -31,7 +31,7 @@ function twentyFourHour(event) {
   let cityThree = document.querySelector("#time-three");
   let cityThreeDate = document.querySelector("#city-three-date");
 
-  cityThree.innerHTML = moment().tz("America/Mexico_City").format("HH:mm:ss");
+  cityThree.innerHTML = moment().tz("America/Mexico_City").format("HH:mm");
   cityThreeDate.innerHTML = moment()
     .tz("America/Mexico_City")
     .format("dddd  Do MMMM YYYY");
@@ -39,7 +39,7 @@ function twentyFourHour(event) {
   let cityFour = document.querySelector("#time-four");
   let cityFourDate = document.querySelector("#city-four-date");
 
-  cityFour.innerHTML = moment().tz("Asia/Bangkok").format("HH:mm:ss");
+  cityFour.innerHTML = moment().tz("Asia/Bangkok").format("HH:mm");
   cityFourDate.innerHTML = moment()
     .tz("Asia/Bangkok")
     .format("dddd  Do MMMM YYYY");
@@ -50,16 +50,45 @@ twentyFour.addEventListener("click", twentyFourHour);
 
 function twelveHour(event) {
   let currentTime = document.querySelector("#current-time");
-  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("h:mm:ss a");
+  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("h:mm a");
   let cityOne = document.querySelector("#time-one");
-  cityOne.innerHTML = moment().tz("Asia/Beirut").format("h:mm:ss a");
+  cityOne.innerHTML = moment().tz("Asia/Beirut").format("h:mm a");
   let cityTwo = document.querySelector("#time-two");
-  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("h:mm:ss a");
+  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("h:mm a");
   let cityThree = document.querySelector("#time-three");
-  cityThree.innerHTML = moment().tz("America/Mexico_City").format("h:mm:ss a");
+  cityThree.innerHTML = moment().tz("America/Mexico_City").format("h:mm a");
   let cityFour = document.querySelector("#time-four");
-  cityFour.innerHTML = moment().tz("Asia/Bangkok").format("h:mm:ss a");
+  cityFour.innerHTML = moment().tz("Asia/Bangkok").format("h:mm a");
 }
 
 let twelve = document.querySelector("#twelve");
 twelve.addEventListener("click", twelveHour);
+
+function updateCity(event) {
+  let chosenCity = event.target.value;
+  if (chosenCity === "current") {
+    chosenCity = moment.tz.guess();
+  }
+  let cityName = chosenCity.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(chosenCity);
+  let citiesElement = document.querySelector("#cities-list");
+  citiesElement.innerHTML = `
+  <div class="row">
+            <div class="col">
+              <h3 class="cityOne" id="city-one">${cityName}</h3>
+              <h4 class="cityOneDate" id="city-one-date">${cityTime.format(
+                "MMMM	Do YYYY"
+              )}</h4>
+            </div>
+            <div class="col">
+              <h3 class="timeOne" id="time-one">${cityTime.format("HH:mm")}</h3>
+            </div>
+                  <hr />
+
+      <a href="#"> <h3 class="allCities" id="all-cities">All Cities</h3></a>
+
+          </div>`;
+}
+
+let citiesSelectElement = document.querySelector("#dropdown");
+citiesSelectElement.addEventListener("change", updateCity);
