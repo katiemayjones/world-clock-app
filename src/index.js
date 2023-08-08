73aaaad516b7a293ceb0a7a9444369965f169673
katiemayjones <katiemayjones1990@gmail.com>
2023-08-08 09:57:23 +0100

    Added seconds and interval

--- src/index.js
+++ src/index.js
@@ -12,15 +12,15 @@ twentyFourHour();
 
 function twentyFourHour(event) {
   let currentTime = document.querySelector("#current-time");
-  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:MM");
+  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("HH:MM:ss");
   let cityOne = document.querySelector("#time-one");
-  cityOne.innerHTML = moment().tz("Asia/Beirut").format("HH:MM");
+  cityOne.innerHTML = moment().tz("Asia/Beirut").format("HH:MM:ss");
   let cityTwo = document.querySelector("#time-two");
-  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("HH:MM");
+  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("HH:MM:ss");
   let cityThree = document.querySelector("#time-three");
-  cityThree.innerHTML = moment().tz("America/Mexico_City").format("HH:MM");
+  cityThree.innerHTML = moment().tz("America/Mexico_City").format("HH:MM:ss");
   let cityFour = document.querySelector("#time-four");
-  cityFour.innerHTML = moment().tz("Asia/Beijing").format("HH:MM");
+  cityFour.innerHTML = moment().tz("Asia/Beijing").format("HH:MM:ss");
 }
 
 let twentyFour = document.querySelector("#twenty-four");
@@ -28,16 +28,18 @@ twentyFour.addEventListener("click", twentyFourHour);
 
 function twelveHour(event) {
   let currentTime = document.querySelector("#current-time");
-  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("h:mm a");
+  currentTime.innerHTML = moment().tz(`${currentLocation}`).format("h:mm:ss a");
   let cityOne = document.querySelector("#time-one");
-  cityOne.innerHTML = moment().tz("Asia/Beirut").format("h:mm a");
+  cityOne.innerHTML = moment().tz("Asia/Beirut").format("h:mm:ss a");
   let cityTwo = document.querySelector("#time-two");
-  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("h:mm a");
+  cityTwo.innerHTML = moment().tz("Europe/Sofia").format("h:mm:ss a");
   let cityThree = document.querySelector("#time-three");
-  cityThree.innerHTML = moment().tz("America/Mexico_City").format("h:mm a");
+  cityThree.innerHTML = moment().tz("America/Mexico_City").format("h:mm:ss a");
   let cityFour = document.querySelector("#time-four");
-  cityFour.innerHTML = moment().tz("Asia/Beijing").format("h:mm a");
+  cityFour.innerHTML = moment().tz("Asia/Beijing").format("h:mm:ss a");
 }
 
 let twelve = document.querySelector("#twelve");
 twelve.addEventListener("click", twelveHour);
+
+setInterval(twentyFourHour, twelveHour, 1000);
